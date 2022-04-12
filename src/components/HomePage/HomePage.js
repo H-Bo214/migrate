06b4b52ca7e61e629acbfa9e6a2a729eb8cebb99
Horacio Borrego <hborrego365@gmail.com>
2022-04-12 12:49:53 -0700

    Create states isLoading, randomCities, selectedRandomCities, error, use axios to fetch urban area data from teleport API

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,36 @@
 import Header from '../Header/Header'
 import Input from '../Input/Input'
 import RandomCities from '../RandomCities/RandomCities'
-const HomePage = () => {
-  return (
+import axios from 'axios'
+import { useState, useEffect, useRef } from 'react'
 
+const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [randomCities, setRandomCities] = useState([])
+  const [selectedRandomCities, setSelectedRandomCities] = useState([])
+  const [error, setError] = useState('')
+  const hasRendered = useRef(false)
+
+  const fetchData = () => {
+    const urbanAreasAPI = 'https://api.teleport.org/api/continents/geonames%3ANA/urban_areas/'
+    if (hasRendered.current === false) {
+      hasRendered.current = true
+      axios.get(urbanAreasAPI)
+      .then(response => setRandomCities(response.data._links['ua:items']))
+      .catch(err => console.error(err))
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const getFourRandomCities = () => {
+
+  }
+  
+  console.log('randomCities: ', randomCities)
+  return (
     <section>
       <Header />
       <Input />
@@ -13,3 +40,14 @@ const HomePage = () => {
 }
 
 export default HomePage
+
+
+  // useEffect(() => {
+  //   const urbanAreasAPI = 'https://api.teleport.org/api/continents/geonames%3ANA/urban_areas/'
+  //   if (hasRendered.current === false) {
+  //     hasRendered.current = true
+  //     fetch(urbanAreasAPI)
+  //     .then(response => response.json())
+  //     .then(data => console.log('data: ', data._links['ua:items']))
+  //   }
+  // }, [])
