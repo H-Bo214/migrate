47a0f504a5e4e751b050edd47ea3c04e8d70e653
Horacio Borrego <hborrego365@gmail.com>
2022-05-09 12:45:41 -0700

    Recieve handleFetchCityDetails as a prop to RandomCity componenet to use in onClick event

--- src/components/RandomCity/RandomCity.js
+++ src/components/RandomCity/RandomCity.js
@@ -1,17 +1,12 @@
 import './RandomCity.css'
-import { useNavigate } from 'react-router-dom'
-const RandomCity = ( { city, imgSrc, cityDetails } ) => {
-  // const navigate = useNavigate()
+const RandomCity = ( { city, imgSrc, handleFetchCityDetails } ) => {
   return (
     <div className='city-img-container'>
       <img 
         className='city-img' 
         alt="city skyline" 
         src={imgSrc}
-        // onClick={() => {
-        //   navigate(`/urbanAreaDetails/${city}`)
-        // }}
-        onClick={() => cityDetails(city)}
+        onClick={() => handleFetchCityDetails(city)}
       />
       <h3 className='city-name'>{city}</h3>
     </div>
