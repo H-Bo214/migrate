11cda1168b8c28ecd90b4d96b7e57c09bece0663
Horacio Borrego <hborrego365@gmail.com>
2022-05-16 11:05:56 -0700

    Remove unnecessary API calls no longer in use, Create state for city select input search, create state for cities available in select drop down menu. Renamed various variables to provide semantic meaning, Add HashLoader spinner icon to display while data is fetched, conditionally render random cities once fetch is complete, Render form for city selection menu, add button to execute fetch once a city is selected.

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,17 +1,25 @@
 import Header from '../Header/Header'
-import Input from '../Input/Input'
 import RandomCities from '../RandomCities/RandomCities'
 import HashLoader from 'react-spinners/HashLoader'
+import Select from 'react-select'
 import { css } from '@emotion/react'
-import { fetchUrbanAreas, fetchImages, fetchFullCityNames } from '../../apiCalls'
-import { generateRandomCities, buildCityObject } from '../../helpers'
+import { fetchUrbanAreas, fetchBatchData, getGeoNameId } from '../../apiCalls'
+import { generateRandomCities, buildCityObject, createOptions } from '../../helpers'
 import { useState, useEffect } from 'react'
+import magnifyingGlass from '../../assets/images/magnifying-glass.svg'
 import './HomePage.css'
 
 const HomePage = () => {
   const [randomCities, setRandomCities] = useState([])
   const [error, setError] = useState('')
   const [isLoading, setIsLoading] = useState(true)
+  const [selectedOption, setSelectedOption] = useState(null)
+  const [urbanAreaList, setUrbanAreaList] = useState([])
+  
+  const spinnerStyle = css`
+    margin-left: 12rem;
+    padding-bottom: 20rem;
+  `
 
   useEffect(() => {
    const getUrbanAreas = async () => {
@@ -19,14 +27,15 @@ const HomePage = () => {
       const urbanAreas = await fetchUrbanAreas()
       if (urbanAreas) {
         const urbanAreasList = urbanAreas._links['ua:items']
-        console.log('urbanAreasList', urbanAreasList)
-        const cityList = generateRandomCities(urbanAreasList)
-        const citySlugUrls = cityList.map(city => city.href)
-        const cityNames = await fetchFullCityNames(citySlugUrls)
-        const cityImageUrls = cityList.map(city => city.href + 'images')
-        const cityImages = await fetchImages(cityImageUrls)
+        const citiesList = createOptions(urbanAreasList)
+        const randomCitiesList = generateRandomCities(urbanAreasList)
+        const citySlugUrls = randomCitiesList.map(city => city.href)
+        const cityNames = await fetchBatchData(citySlugUrls)
+        const cityImageUrls = randomCitiesList.map(city => city.href + 'images')
+        const cityImages = await fetchBatchData(cityImageUrls)
         const images = cityImages.flatMap(el => el.photos)
         const results = buildCityObject(cityNames, images)
+        setUrbanAreaList(citiesList)
         setIsLoading(false)
         setRandomCities(results)
       }
@@ -37,23 +46,54 @@ const HomePage = () => {
    getUrbanAreas()
   }, [])
 
-  const spinnerStyle = css`
-  margin-left: 12rem;
-  padding-bottom: 20rem;
-`;
+const handleSelectedOption = (e) => {
+  const { value } = e
+  setSelectedOption(value)
+}
+
+const handleSearch = async (e) => {
+  e.preventDefault()
+  const slugEndPoint = `https://api.teleport.org/api/urban_areas/slug:${selectedOption}/`
+  const slugData = await getGeoNameId(slugEndPoint)
+  const geoNameIdEndPoint = slugData._links['ua:identifying-city'].href
+  const scoresEndPoint = `https://api.teleport.org/api/urban_areas/slug:${selectedOption}/scores`
+  const imagesEndPoint = `https://api.teleport.org/api/urban_areas/slug:${selectedOption}/images`
+  const dataEndPoints = [slugEndPoint, scoresEndPoint, geoNameIdEndPoint, imagesEndPoint]
+  const cityData = await fetchBatchData(dataEndPoints)
+  console.log('cityData in handleSearch', cityData)
+}
 
   return (
     <section className='main-content'>
       {error && <h1>{error}</h1>}
       <Header />
-      <Input />
+      <form className='select-search-container'>
+        <button className='search-button'>
+          <img 
+          alt='blue magnifying glass icon' 
+          src={magnifyingGlass} 
+          onClick={handleSearch}
+          className='search-icon'
+          />
+        </button>
+        <Select 
+          defaultValue={selectedOption}
+          onChange={handleSelectedOption}
+          options={urbanAreaList}
+          className='select-menu'
+          placeholder='Search or select a city'
+        />
+      </form>
       {isLoading ? 
-      <HashLoader color='#2E84FF' loading={isLoading} size={100} css={spinnerStyle}/>  :
+      <HashLoader 
+        color='#2E84FF' 
+        loading={isLoading} 
+        size={100} 
+        css={spinnerStyle}/>  :
       <RandomCities  
         cityList={randomCities}  
       />
     }
-
     </section>
   )
 }
