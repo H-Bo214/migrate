7aee2f5454759fd00f07752f51745db17d038d5c
Horacio Borrego <hborrego365@gmail.com>
2022-05-16 11:08:53 -0700

    Update getCityDetails fetch call to use fetchBatchData call. Add classname to main element in RandomCities.js

--- src/components/RandomCities/RandomCities.js
+++ src/components/RandomCities/RandomCities.js
@@ -1,14 +1,16 @@
 import RandomCity from "../RandomCity/RandomCity"
 import { cleanData, makeCityUrlsArr } from '../../helpers'
 import { useNavigate } from 'react-router-dom'
-import {  getCityDetails } from '../../apiCalls'
+import {  fetchBatchData } from '../../apiCalls'
+import './RandomCities.css'
 const RandomCities = ( { cityList } ) => {
   const navigate = useNavigate()
   
   const handleFetchCityDetails = async (cityName) => {
     const cityToGet = cityList.find(city => city.name === cityName)
     const urlArray = makeCityUrlsArr(cityToGet)
-    const fetchedCityDetails = await getCityDetails(urlArray)
+    const fetchedCityDetails = await fetchBatchData(urlArray)
+    console.log('fetchCityDetails', fetchedCityDetails)
     const cityData = cleanData(fetchedCityDetails, cityToGet.image)
     navigate(`/urbanAreaDetails/${cityToGet.name}`, {state: cityData})
   }
@@ -24,7 +26,7 @@ const RandomCities = ( { cityList } ) => {
 
   return (
     <section className="main-container">
-      <main>
+      <main className="main-content-random-cities">
         <h2 className="greeting">Check out these cities</h2>
         {allCityNames}
       </main>
