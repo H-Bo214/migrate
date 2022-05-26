40f2feec2879e617cdc36e4b71fd230e02b34450
Horacio Borrego <hborrego365@gmail.com>
2022-05-26 10:49:41 -0700

    Add header test to confirm the presence of the H1 heading

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -4,13 +4,22 @@ import Header from '../Header/Header'
 import { MemoryRouter } from 'react-router-dom'
 
 describe('Header', () => {
-  it('should render the Header on page load',  () => {
+  beforeEach(() => {
     render(
       <MemoryRouter>
         <Header />
       </MemoryRouter>
     )
+  })
+
+  it('should render the Migrate logo', () => {
     const img = screen.getByRole('img', { name: 'Blue gradient circle with a geometric bird in the center.'})  
     expect(img).toBeInTheDocument()
   })
+
+  it('should render a heading', () => {
+    const heading = screen.getByRole('heading', { name: /where do you want to live\?/i })
+    expect(heading).toBeInTheDocument()
+  })
+  
 })
