f2ca493092d03dfcc2b66c59bfe5cd9f207dc6d6
Horacio Borrego <hborrego365@gmail.com>
2022-06-01 12:08:54 -0700

    Add test to UrbanAreaImage.js

---
+++ src/components/UrbanAreaImage/UrbanAreaImage.test.js
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import UrbanAreaImage from '../UrbanAreaImage/UrbanAreaImage'
+import { MemoryRouter } from 'react-router-dom'
+
+describe('UrbanAreaImage', () => {
+  it ('should render a city image',  () => {
+    const cityName = 'Ottawa, Canada'
+    const img = 'https://d13k13wj6adfdf.cloudfront.net/urban_areas/ottawa-b78a66e47b.jpg'
+
+    render(
+      <UrbanAreaImage 
+        cityName={cityName}
+        img={img}
+      />, {wrapper: MemoryRouter}
+    )
+    const cityImg = screen.getByRole('img', {  name: /ottawa, canada skyline/i})
+    expect(cityImg).toBeInTheDocument()
+  })
+
+  it ('should render a back button icon',  () => {
+    const cityName = 'Ottawa, Canada'
+    const img = 'https://d13k13wj6adfdf.cloudfront.net/urban_areas/ottawa-b78a66e47b.jpg'
+
+    render(
+      <UrbanAreaImage 
+        cityName={cityName}
+        img={img}
+      />, {wrapper: MemoryRouter}
+    )
+    const backArrow = screen.getByRole('img', {  name: /back button icon/i})
+    expect(backArrow).toBeInTheDocument()
+  })
+
+})
